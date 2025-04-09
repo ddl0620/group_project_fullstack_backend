772b0d41010249dea939b6eae718b306dff354b7
Brian Tran <100t.baron@gmail.com>
2025-04-09 19:13:30 +0700

    Update notification.interfaces.ts

--- interfaces/notification.interfaces.ts
+++ interfaces/notification.interfaces.ts
@@ -1,10 +1,22 @@
-import mongoose, {Document} from "mongoose";
-import {NotificationType} from "../enums/notificationType.enums";
-import {UserInterface} from "./user.interfaces";
-import {EventInterface} from "./event.interfaces";
-import {IParticipantStatus} from "./participant.interfaces";
+import mongoose, { Document } from "mongoose";
+import { NotificationType } from "../enums/notificationType.enums";
+import { UserInterface } from "./user.interfaces";
+import { EventInterface } from "./event.interfaces";
 
 export interface NotificationInterface extends Document {
-    // User ID of the recipient
-    //
+    recipient: mongoose.Schema.Types.ObjectId | UserInterface; // User receiving the notification
+
+    type: NotificationType; // Type of notification (enum)
+    
+    message: string; // Notification message
+
+    relatedEvent?: mongoose.Schema.Types.ObjectId | EventInterface; // Event Invitation, event reminder, event cancellation
+    
+    relatedUser?: mongoose.Schema.Types.ObjectId | UserInterface; // Friend request, friend request accepted, friend request rejected, message received
+    // Evenvitation sent by a user to another user used both
+    isRead: boolean; // Whether the notification has been read
+    
+    createdAt: Date; // Timestamp when the notification was created
+    
+    updatedAt?: Date; // Timestamp when the notification was last updated
 }
